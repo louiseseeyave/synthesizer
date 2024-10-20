1213dd0fd7481249e483d0891a5c0efee8846950
louiseseeyave <louise.seeyave@gmail.com>
2024-10-20 20:49:02 +0100

    new parametric sfh rebinning method

--- src/synthesizer/load_data/load_scsam.py
+++ src/synthesizer/load_data/load_scsam.py
@@ -6,9 +6,9 @@ Currently implemented are loading methods for
 """
 
 import numpy as np
-from scipy.interpolate import NearestNDInterpolator as NNI
-from scipy.interpolate import RegularGridInterpolator as RGI
 from unyt import Msun, yr
+
+from regrid_sfh import get_grid_map, rebin_grid
 
 from synthesizer.parametric.galaxy import Galaxy as ParametricGalaxy
 from synthesizer.parametric.stars import Stars as ParametricStars
@@ -16,8 +16,10 @@ from synthesizer.particle.galaxy import Galaxy as ParticleGalaxy
 from synthesizer.particle.stars import Stars as ParticleStars
 
 
-def load_SCSAM(fname, method, grid=None, verbose=False):
-    r"""
+def load_SCSAM(
+        fname, method, redshift, cosmo, grid=None, grid_map=None, verbose=False
+):
+    """
     Read an SC-SAM star formation data file.
 
     Returns a list of galaxy objects, halo indices, and birth halo IDs.
@@ -27,14 +29,20 @@ def load_SCSAM(fname, method, grid=None, verbose=False):
         fname (str):
             The SC-SAM star formation data file to be read.
         method (str):
-            'particle', 'parametric_NNI' or 'parametric_RGI', depending on how
-            you wish to model your SFZH. 'particle' treats each age-Z bin as a
-            particle. 'parametric_NNI' uses scipy's nearest ND interpolator to
-            interpolate the grid for a parametric SFH 'parametric_RGI' uses
-            scipy's regular grid interpolator to interpolate the grid for a
-            parametric SFH.
+            How to model SFH. Options: 'particle' or 'parametric'
+            'particle' treats each age-Z bin as a particle.
+            'parametric' rebins the SFH grid to the SPS grid binning
+        redshift (float):
+            Redshift of the galaxies that will be loaded
+        cosmo (object):
+            Astropy cosmology object
         grid (grid object):
-            Grid object to extract from (needed for parametric galaxies).
+            Grid object to extract from (needed for parametric method).
+        grid_map (dict):
+            Dictionary that contains information for mapping the old,
+            original SFH grid to the desired new grid (needed for
+            parametric method). Obtained from get_grid_map function in
+            regrid_sfh.py. 
         verbose (bool):
             Are we talking?
 
@@ -124,19 +132,25 @@ def load_SCSAM(fname, method, grid=None, verbose=False):
         if (count - 3) % (age_len + 1) == age_len and count > 3:
             count_gal += 1
 
+            # Convert SFH units
+            SFH = np.array(SFH) * 10**9  # Msun
+
+            # Convert age axis of SFH grid from age of the universe to stellar ages
+            SFH = _universe_to_stellar_age(
+                SFH, old_age, redshift, cosmo, check_bin=True, verbose=True
+            )
+
             # Create galaxy object
             if method == "particle":
                 galaxy = _load_SCSAM_particle_galaxy(
                     SFH, age_lst, Z_lst, verbose=verbose
                 )
-            elif method == "parametric_NNI":
+            elif method == "parametric":
                 galaxy = _load_SCSAM_parametric_galaxy(
-                    SFH, age_lst, Z_lst, "NNI", grid, verbose=verbose
+                    SFH, age_lst, Z_lst, grid, grid_map, verbose=verbose
                 )
-            elif method == "parametric_RGI":
-                galaxy = _load_SCSAM_parametric_galaxy(
-                    SFH, age_lst, Z_lst, "RGI", grid, verbose=verbose
-                )
+            else:
+                raise ValueError("Method not recognised.")
 
             # Append to list of galaxy objects
             galaxies.append(galaxy)
@@ -149,10 +163,11 @@ def load_SCSAM(fname, method, grid=None, verbose=False):
 def _load_SCSAM_particle_galaxy(SFH, age_lst, Z_lst, verbose=False):
     """
     Treat each age-Z bin as a particle.
+    PSA: From what I recall, this method does not work very well.
 
     Args:
     SFH: age x Z SFH array as given by SC-SAM for a single galaxy
-    age_lst: age bins in the SFH array (Gyr)
+    age_lst: age bins in the SFH array (yr)
     Z_lst: metallicity bins in the SFH array (unitless)
     """
 
@@ -173,14 +188,14 @@ def _load_SCSAM_particle_galaxy(SFH, age_lst, Z_lst, verbose=False):
             if SFH[age_ind][Z_ind] == 0:
                 continue
             else:
-                p_imass.append(SFH[age_ind][Z_ind])  # 10^9 Msun
+                p_imass.append(SFH[age_ind][Z_ind])  # Msun
                 p_age.append(age_lst[age_ind])  # Gyr
                 p_Z.append(Z_lst[Z_ind])  # unitless
 
     # Convert units
     if verbose:
         print("Converting units...")
-    p_imass = np.array(p_imass) * 10**9  # Msun
+    p_imass = np.array(p_imass)  # Msun
     p_age = np.array(p_age) * 10**9  # yr
     p_Z = np.array(p_Z)  # unitless
 
@@ -201,13 +216,12 @@ def _load_SCSAM_particle_galaxy(SFH, age_lst, Z_lst, verbose=False):
 
 
 def _load_SCSAM_parametric_galaxy(
-    SFH, age_lst, Z_lst, method, grid, verbose=False
+        SFH, age_lst, Z_lst, grid, grid_map=None, verbose=False
 ):
     """
     Obtain galaxy SED using the parametric method.
     This is done by interpolating the grid.
     Returns a galaxy object.
-    Adapted from code by Kartheik Iyer.
 
     Args:
     SFH: age x Z SFH array as given by SC-SAM for a single galaxy
@@ -218,47 +232,28 @@ def _load_SCSAM_parametric_galaxy(
             'RGI' - scipy's regular grid interpolator
     """
 
-    # This the grid that we want to interpolate to
+    # SPS grid that we want to regrid to
     new_age = 10**grid.log10age  # yr
     new_Z = np.log10(grid.metallicity)  # log10Z
+    new_age_edges, new_Z_edges = _get_sps_bin_edges(grid)
+    new_grid_dim = (len(new_age), len(new_Z))
 
-    # This is the old grid, to be interpolated
+    # Original SFH grid output by SAM
     old_age = np.array(age_lst) * 10**9  # yr
     old_Z = np.log10(Z_lst)  # log10Z
+    old_age_edges, old_Z_edges = _get_scsam_bin_edges(
+        old_age, old_Z, verbose=True
+    )
 
-    # Convert SFH units
-    SFH = np.array(SFH) * 10**9  # Msun
-    # sum_SFH = np.log10(np.sum(SFH))
-
-    # Using regular grid interpolator
-    if method == "RGI":
-        # Get coords for new grid
-        new_X, new_Y = np.meshgrid(new_age, new_Z, indexing="ij")
-        # Set up the old grid for interpolation
-        interp_obj = RGI(
-            points=(old_age, old_Z), values=SFH, bounds_error=False
+    # If the grid_map object is not specified, create it
+    if grid_map==None:
+        grid_map = get_grid_map(
+            old_age_edges, old_Z_edges, new_age_edges,
+            new_Z_edges, old_age, old_Z,
         )
-        # Interpolate to new grid coordinates and reshape
-        new_SFH = interp_obj((new_X.ravel(), new_Y.ravel()))
-        new_SFH = new_SFH.reshape(len(new_age), len(new_Z))
-        # Convert NaNs to zero
-        new_SFH[np.isnan(new_SFH)] = 0.0
 
-    # Using nearest ND interpolator
-    if method == "NNI":
-        # Get coords for new grid
-        new_X, new_Y = np.meshgrid(new_age, new_Z, indexing="ij")
-        # Get list of coordinates of old grid
-        old_X, old_Y = np.meshgrid(old_age, old_Z, indexing="ij")
-        old_coords = np.vstack([old_X.ravel(), old_Y.ravel()]).T
-        # Set up the old grid for interpolation
-        interp_obj = NNI(old_coords, SFH.flatten(), rescale=True)
-        # Interpolate to new grid
-        new_SFH = interp_obj(new_X, new_Y)
-
-    # Normalise the new grid
-    norm_SFH = np.sum(SFH) / np.sum(new_SFH)
-    new_SFH *= norm_SFH
+    # Rebin the SFH grid
+    new_SFH = rebin_grid(SFH, grid_map, new_grid_dim)
 
     # Create Binned SFZH object
     stars = ParametricStars(
@@ -271,3 +266,220 @@ def _load_SCSAM_parametric_galaxy(
     parametric_galaxy = ParametricGalaxy(stars)
 
     return parametric_galaxy
+
+
+def _universe_to_stellar_age(
+        SFH,
+        age_bins,
+        redshift,
+        cosmo,
+        check_bins=False,
+        verbose=False,
+):
+
+    """
+    Convert the age axis of an SFH grid from age of the universe
+    to stellar ages. Meant for SC-SAM SFHs.
+    
+    Args:
+        SFH (array): 
+            SFH array for a single galaxy (Msun). Note that the age axis
+            of this SFH array should be the age of the universe (yr).
+        age_bins (array): 
+            Bin centres of the SFH grid age axis in age of the universe (yr)
+        redshift (float): 
+            Redshift of the galaxy
+        cosmo (object):
+            Astropy FlatLambdaCDM object. Note that the conversion from
+            age of the universe to stellar ages is sensitive to cosmology.
+        check_bins (bool):
+            Check whether the total stellar mass is conserved?
+        verbose (bool):
+            Increase output verbosity
+                
+    Returns:
+        new_SFH (array):
+            SFH array with the age axis in the age of the stellar 
+            particles (Msun)
+        new_age_bins (array):
+            Bin centres of the new age axis in stellar years (yr)
+    """
+ 
+    # Get age of universe at the redshift of the galaxy
+    snapshot_age = cosmo.age(redshift).value * 1e9 # yr
+    if verbose:
+        print(f'The universe was {snapshot_age/(1e9)} Gyr old at z={redshift}.')
+        
+    # Keep the age bin centres equal to or below the age of the
+    # universe at the galaxy's redshift
+    keep_bins = np.where(age_bins <= snapshot_age)[0]
+    
+    # As we are working with bin centres rather than bin edges, it is
+    # possible that stellar mass is formed in an age bin with a centre
+    # that is older than the given redshift
+    keep_bins = np.append(keep_bins, keep_bins[-1]+1)
+
+    # Mask the age bin centres
+    new_age_bins = age_bins[keep_bins]
+    if verbose:
+        print(f'Relevant bin centres in age of the universe (yr): {new_age_bins}')
+  
+    # Check that stellar mass is conserved
+    if check_bins:
+        
+        # Discard age bin centres greater than the age of the universe
+        # at the galaxy's redshift
+        exclude_bins = np.where(age_bins > snapshot_age)[0]
+        # Exclude first bin in this list, which may contain some mass
+        exclude_bins = exclude_bins[1:]
+        print(f'Discarded bin centres are (yr): {age_bins[exclude_bins]}')
+        
+        # Check if total mass is conserved
+        old_sfh_mass = np.sum(np.array(SFH))
+        new_sfh_mass = np.sum(np.array(SFH)[keep_bins])
+        print(f'Original SFH grid total mass: {new_sfh_mass:5e} Msun')
+        print(f'New SFH grid total mass: {new_sfh_mass:5e} Msun')
+        if np.abs(old_sfh_mass-new_sfh_mass)<1e-10:
+            print('Mass in the SFH grid is conserved!')
+        else:
+            raise ValueError('Total mass is not conserved.')
+        
+        # The sum of the discarded SFH should be equal to zero
+        discarded_mass = np.sum(np.array(SFH)[exclude_bins])
+        print(f'Discarded SFH grid total mass: {discarded_mass} Msun')
+        if np.sum(np.array(SFH)[exclude_bins]) == 0:
+            print('Discarded bins are empty (as should be)!')
+        else:
+            raise ValueError('Discarded bins have non-zero mass.')
+        
+    # Convert age of the universe to stellar age
+    new_age_bins = snapshot_age - new_age_bins
+    
+    # Make the final age bin equal to zero - we don't want negative ages
+    # Note: final bin because the stellar ages are now in descending order
+    # Technically, the bin centre should not be zero, but it does not
+    # really matter so long as we get the bin edges right later
+    new_age_bins = np.append(new_age_bins[:-1], 0.)
+    if verbose:
+        print(f'Bin centres in stellar ages (yr): {new_age_bins}')
+
+    # Mask the SFH array
+    new_SFH = np.array(SFH)[keep_bins]
+    if verbose:
+        print(f'The new SFH has shape {SFH.shape}.')
+
+    # Flip arrays to be in order of ascending stellar age
+    new_age_bins = np.flip(new_age_bins)
+    new_SFH = np.flipud(new_SFH)
+
+    return new_SFH, new_age_bins
+
+
+def _get_scsam_bin_edges(
+        age_bins,
+        log10metal_bins,
+        verbose=True,
+):
+
+    """
+    Get bin edges of the age and metallicity axes.
+    Specifically for SC-SAM SFH grids, which are uniformly-spaced along
+    the age axis and irregularly-spaced along the metallicity axis.
+
+    Args:
+        age_bins:
+            Bin centres of the SFH grid age axis in stellar years (yr)
+            Note: must be uniformly-spaced!
+        log10metal_bins:
+            Bin centres of the SFH grid metallicity axis in log10Z
+        verbose:
+            Increase output verbosity
+
+    Returns:
+        age_edges:
+            Bin edges of the SFH grid age axis in stellar years (yr)
+        log10metal_edges:
+            Bin edges of the SFH grid metallicity axis in log10Z  
+    """
+
+    # Get stellar age bin edges
+    # Make the following assumptions:
+    # 1. Lowest bin edge is 0
+    # 2. Second lowest bin edge is equal to the second lowest bin
+    #    centre minus half the bin width
+    # 3. Highest bin edge is highest bin + half the bin width
+    # 4. The remaining bin edges lie equidistant between bin centres
+    binw = np.array(age_bins)[-1] - np.array(age_bins)[-2]
+    age_edges = np.empty(len(age_bins)+1)
+    age_edges[0] = 0.
+    age_edges[1] = sam_age_stars[1]-binw/2
+    age_edges[2:] = np.array(sam_age_stars)[1:] + binw/2
+    if verbose:
+        print(f'The stellar age bin edges are: {age_edges}')
+
+    # Get log metallicity bin edges
+    # Make the following assumptions:
+    # 1. Lowest bin edge is the lowest bin centre
+    # 2. Highest bin edge is the highest bin centre
+    # 3. The remaining bin edges lie equidistant between bin centres
+    log10metal_edges = np.empty(len(log10metal_bins)+1)
+    log10metal_edges[0] = log10metal_bins[0]
+    log10metal_edges[-1] = log10metal_bins[-1]
+    log10metal_bins[1:-1] = np.array(log10metal_bins)[:-1] + \
+        np.diff(log10metal_bins)/2
+    if verbose:
+        print(f'The log10Z bin edges are: {log10metal_edges}')
+
+    return age_edges, log10metal_edges
+
+
+def _get_sps_bin_edges(
+        grid,
+):
+
+    """
+    Get bin edges of an SPS grid.
+
+    Args:
+        grid (object):
+            Synthesizer grid object
+
+    Returns:
+        age_edges:
+            Bin edges of the SFH grid age axis in stellar years (yr)
+        log10metal_edges:
+            Bin edges of the SFH grid metallicity axis in log10Z
+    """
+
+    # Get stellar age bin centres
+    age_bins = 10**grid.log10age # yr
+
+    # Get stellar age bin edges
+    # Make the following assumptions:
+    # 1. Lowest bin edge is 0
+    # 2. Highest bin edge is the highest bin centre
+    # 3. The remaining bin edges lie equidistant between bin centres
+    age_edges = np.empty(len(age_bins)+1)
+    age_edges[0] = 0.
+    age_edges[-1] = age_bins[-1]
+    age_bins[1:-1] = np.array(age_bins)[:-1] + np.diff(age_bins)/2
+    if verbose:
+        print(f'The stellar age bin edges are: {age_edges}')
+
+    # Get log metallicity bin centres
+    log10metal_bins = np.log10(grid.metallicity)
+        
+    # Get log metallicity bin edges
+    # Make the following assumptions:
+    # 1. Lowest bin edge is the lowest bin centre
+    # 2. Highest bin edge is the highest bin centre
+    # 3. The remaining bin edges lie equidistant between bin centres
+    log10metal_edges = np.empty(len(log10metal_bins)+1)
+    log10metal_edges[0] = log10metal_bins[0]
+    log10metal_edges[-1] = log10metal_bins[-1]
+    log10metal_bins[1:-1] = np.array(log10metal_bins)[:-1] + \
+        np.diff(log10metal_bins)/2
+    if verbose:
+        print(f'The log10Z bin edges are: {log10metal_edges}')
+            
+    return age_edges, log10metal_edges
---
+++ src/synthesizer/load_data/regrid_sfh.py
@@ -0,0 +1,245 @@
+import numpy as np
+
+
+def get_grid_map(
+        old_age_edges,
+        old_metal_edges,
+        new_age_edges,
+        new_metal_edges,
+        old_age_centres=None,
+        old_metal_centres=None,
+):
+    
+    """
+    Obtain a mapping to map mass from one SFH grid (old) to another (new).
+    Typically, the old grid would be the SAM SFH grid and the new grid
+    would be the SPS grid.
+
+    Args:
+        old_age_edges (array):
+            Bin edges of the old SFH grid stellar age axis
+        old_metal_edges (array):
+            Bin edges of the old SFH grid metallicity axis
+        new_age_edges (array):
+            Bin edges of the new SFH grid stellar age axis
+        new_metal_edges (array):
+            Bin edges of the new SFH grid metallicity axis
+        old_age_centres (array):
+            Bin centres of the old SFH grid stellar age axis
+            Optional - saved in the output dictionary for reference.
+        old_metal_centres (array):
+            Bin centres of the old SFH grid metallicity axis
+            Optional - saved in the output dictionary for reference.
+
+    Returns:
+        map_dict (dict):
+            Dictionary that contains information for mapping the old,
+            original SFH grid to the desired new grid.
+    """
+            
+    # Get lengths
+    old_age_length = np.diff(old_age_edges)
+    old_metal_lengths = np.diff(old_metal_edges)
+    new_age_length = np.diff(new_age_edges)
+    new_metal_lengths = np.diff(new_metal_edges)
+
+    # Create dictionary to store mapping
+    map_dict = {}
+    
+    # Loop over each bin on the metallicity axis of the old grid
+    map_dict['metal_bin_index'] = {}
+    for ii in range(len(old_metal_lengths)):
+
+        map_dict['metal_bin_index'][ii] = {}
+
+        # Get the upper and lower bound of this bin
+        old_low = old_metal_edges[ii]
+        old_up = old_metal_edges[ii+1]
+        old_length = old_metal_lengths[ii]
+
+        # To find overlapping bins in the new grid, identify bins in
+        # the new grid with:
+        # 1. A lower bound that falls below the upper bound of the old bin
+        # 2. An upper bound that lies above the lower bound of the old bin
+        ok_low = (new_metal_edges[:-1] < old_up)
+        ok_high = (new_metal_edges[1:] > old_low)
+        ok_ind = np.where(ok_low & ok_high)[0]
+
+        # Along the metallicity axis, get the fraction of the old bin's
+        # width that is occupied by each overlapping new bin
+        # If there is only one bin in the new grid that overlaps, then
+        # the fraction is simply 1
+        if len(ok_ind)==1:
+            frac = [1]
+        # If there are at at least two bins in the new grid that overlap,
+        # calculate the overlap of the two end bins that bracket the old bin 
+        elif len(ok_ind)>=2:
+            frac = np.ones(len(ok_ind))
+            # Find the overlapping length of the lower bin 
+            l1 = new_metal_edges[ok_ind[0]+1] - old_low
+            l1_frac = l1/old_length
+            frac[0] = l1_frac
+            #  Find the overlapping length of the higher bin
+            l2 = old_up - new_metal_edges[ok_ind[-1]]
+            l2_frac = l2/_sam_length
+            frac[-1] = l2_frac
+            # Calculate the overlap of any remaining bins
+            if len(ok_ind)>2:
+                for iii, ind in enumerate(ok_ind[1:-1]):
+                    frac[1+iii] = new_metal_lengths[ind]/old_length
+        # If no bin in the new grid overlaps with this old bin...
+        elif len(ok_ind)==0:
+            # Could be that the old grid goes to higher values than the new grid
+            if np.sum(ok_high)==0:
+                # In this case, move all mass to highest bin in the new grid
+                ok_ind = [len(new_metal_lengths)]
+                frac = [1]
+            # Or could be the old grid goes to lower values than the new grid
+            elif np.sum(ok_low)==0:
+                # In this case, move all mass to lowest bin in the new grid
+                ok_ind = [0]
+                frac = [1]
+            else:
+                print('There is an edge case that has not been considered.')
+                break
+        else:
+            print('Something is very wrong.')
+            break
+
+        # print('sum of frac:', np.sum(frac))
+
+        # Store values
+        map_dict['metal_bin_index'][ii]['new_grid_indices'] = np.array(ok_ind)
+        map_dict['metal_bin_index'][ii]['fraction'] = np.array(frac)
+        if old_metal_centres!=None:
+            map_dict['metal_bin_index'][ii]['old_bin_value'] = old_metal_centres[ii]
+
+
+    # Loop over each bin on the age axis of the old grid
+    # Exactly the same procedure as before. To do: rewrite to loop over axes
+    map_dict['age_bin_index'] = {}
+    for ii in range(len(old_age_lengths)):
+
+        map_dict['age_bin_index'][ii] = {}
+
+        # Get the upper and lower bound of this bin
+        old_low = old_age_edges[ii]
+        old_up = old_age_edges[ii+1]
+        old_length = old_age_lengths[ii]
+
+        # To find overlapping bins in the new grid, identify bins in
+        # the new grid with:
+        # 1. A lower bound that falls below the upper bound of the old bin
+        # 2. An upper bound that lies above the lower bound of the old bin
+        ok_low = (new_age_edges[:-1] < old_up)
+        ok_high = (new_age_edges[1:] > old_low)
+        ok_ind = np.where(ok_low & ok_high)[0]
+
+        # Along the age axis, get the fraction of the old bin's
+        # width that is occupied by each overlapping new bin
+        # If there is only one bin in the new grid that overlaps, then
+        # the fraction is simply 1
+        if len(ok_ind)==1:
+            frac = [1]
+        # If there are at at least two bins in the new grid that overlap,
+        # calculate the overlap of the two end bins that bracket the old bin 
+        elif len(ok_ind)>=2:
+            frac = np.ones(len(ok_ind))
+            # Find the overlapping length of the lower bin 
+            l1 = new_age_edges[ok_ind[0]+1] - old_low
+            l1_frac = l1/old_length
+            frac[0] = l1_frac
+            #  Find the overlapping length of the higher bin
+            l2 = old_up - new_age_edges[ok_ind[-1]]
+            l2_frac = l2/_sam_length
+            frac[-1] = l2_frac
+            # Calculate the overlap of any remaining bins
+            if len(ok_ind)>2:
+                for iii, ind in enumerate(ok_ind[1:-1]):
+                    frac[1+iii] = new_age_lengths[ind]/old_length
+        # If no bin in the new grid overlaps with this old bin...
+        elif len(ok_ind)==0:
+            # Could be that the old grid goes to higher values than the new grid
+            if np.sum(ok_high)==0:
+                # In this case, move all mass to highest bin in the new grid
+                ok_ind = [len(new_age_lengths)]
+                frac = [1]
+            # Or could be the old grid goes to lower values than the new grid
+            elif np.sum(ok_low)==0:
+                # In this case, move all mass to lowest bin in the new grid
+                ok_ind = [0]
+                frac = [1]
+            else:
+                print('There is an edge case that has not been considered.')
+                break
+        else:
+            print('Something is very wrong.')
+            break
+
+        # print('sum of frac:', np.sum(frac))
+
+        # Store values
+        map_dict['age_bin_index'][ii]['new_grid_indices'] = np.array(ok_ind)
+        map_dict['age_bin_index'][ii]['fraction'] = np.array(frac)
+        if old_age_centres!=None:
+            map_dict['age_bin_index'][ii]['old_bin_value'] = old_age_centres[ii]
+        
+    return map_dict
+
+
+def rebin_grid(
+        SFH,
+        grid_map,
+        new_grid_dim,
+):
+    
+    """
+    Rebin a parametric SFH grid from one set of age-metallicity bin
+    centres to another.
+
+    Args:
+        SFH (array):
+            Original SFH grid to be rebinned.
+        grid_map (dict):
+            Dictionary that contains information for mapping the old,
+            original SFH grid to the desired new grid. Obtained from
+            the get_grid_map function in this script.
+        new_grid_dim (array):
+            Dimensions of the new grid
+
+    Returns:
+       new_SFH
+    """
+        
+    # Empty SPS grid, to be populated
+    new_SFH = np.zeros(new_grid_dim)
+
+    # Get i,j coordinates of old SFH
+    xx = np.arange(0, np.array(SFH).shape[0], 1)
+    yy = np.arange(0, np.array(SFH).shape[1], 1)
+    iis, jjs = np.meshgrid(xx, yy, indexing='ij')
+
+    # Only perform mass distribution for bins with non-zero mass
+    keep = SFH > 0.
+    iis = iis[keep].flatten()
+    jjs = jjs[keep].flatten()
+        
+    # Loop over each bin in the old grid with non-zero mass
+    for (i, j) in zip(iis, jjs):
+
+        # Metallicity axis
+        new_metal_ind = grip_map['metal_bin_index'][j]['new_grid_indices']
+        new_metal_frac = grid_map['metal_bin_index'][j]['fraction']
+
+        # Age axis
+        new_age_ind = grid_map['age_bin_index'][i]['new_grid_indices']
+        new_age_frac = grid_map[f'age_bin_index'][i]['fraction']
+
+        # Loop over each relevant SPS bin
+        for (new_i, frac_i) in zip(new_age_ind, new_age_frac):
+            for (new_j, frac_j) in zip(new_metal_ind, new_metal_frac):
+
+                frac = frac_i * frac_j
+                new_SFH[new_i,new_j] += SFH[i,j] * frac
+        
+    return new_SFH
